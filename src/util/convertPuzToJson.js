5f96d28f00a18ababcb7357a406fcc92860d44f6
Peter Jablonski <pwjablonski@gmail.com>
2019-08-06 12:07:16 -0400

    update puz reader

--- src/util/convertPuzToJson.js
+++ src/util/convertPuzToJson.js
@@ -1,83 +1,82 @@
 function isPuz(bytes) {
-  const magic = "ACROSS&DOWN";
-  for (let i = 0; i < magic.length; i += 1) {
-    if (bytes[2 + i] !== magic.charCodeAt(i)) return false;
+  const label = "ACROSS&DOWN";
+  for (let i = 0; i < label.length; i += 1) {
+    if (bytes[2 + i] !== label.charCodeAt(i)) return false;
   }
-  return bytes[2 + magic.length] === 0;
+  return bytes[2 + label.length] === 0;
 }
 
-class PuzReader {
-  constructor(buf) {
-    this.buf = buf;
+function readString(buf, pos) {
+  const result = [];
+  while (true) {
+    pos += 1;
+    const c = buf[pos];
+    if (c === 0) break;
+    result.push(String.fromCodePoint(c));
   }
+  return { string: result.join(""), pos };
+}
 
-  readShort(ix) {
-    // eslint-disable-next-line
-    return this.buf[ix] | (this.buf[ix + 1] << 8);
+function puzToJson(bytes) {
+  const buf = bytes;
+  let pos;
+  const json = {};
+  const w = buf[0x2c];
+  const h = buf[0x2d];
+
+  json.size = { cols: w, rows: h };
+  const grid = [];
+  for (let i = 0; i < w * h; i += 1) {
+    grid.push(String.fromCodePoint(buf[0x34 + i]));
   }
+  json.grid = grid;
+  pos = 0x33 + 2 * w * h;
+  let result = readString(buf, pos);
+  json.title = result.string;
+  pos = result.pos;
+  result = readString(buf, pos);
+  json.author = result.string;
+  pos = result.pos;
+  result = readString(buf, pos);
+  json.copyright = result.string;
+  pos = result.pos;
 
-  readString() {
-    const result = [];
-    while (true) {
-      const c = this.buf[(this.ix += 1)];
-      if (c === 0) break;
-      result.push(String.fromCodePoint(c));
+  const across = [];
+  const down = [];
+  const gridnums = [];
+  let label = 1;
+  for (let i = 0; i < w * h; i += 1) {
+    if (grid[i] === ".") {
+      gridnums.push(0);
+      continue;
     }
-    return result.join("");
+    let inc = 0;
+    if (i % w === 0 || grid[i - 1] === ".") {
+      result = readString(buf, pos);
+      pos = result.pos;
+      across.push(`${label}. ${result.string}`);
+      inc = 1;
+    }
+    if (i < w || grid[i - w] === ".") {
+      result = readString(buf, pos);
+      pos = result.pos;
+      down.push(`${label}. ${result.string}`);
+      inc = 1;
+    }
+
+    if (i % w === 0 || grid[i - 1] === ".") {
+      gridnums.push(label);
+    } else if (i < w || grid[i - w] === ".") {
+      gridnums.push(label);
+    } else {
+      gridnums.push(0);
+    }
+
+    label += inc;
   }
-
-  toJson() {
-    const json = {};
-    const w = this.buf[0x2c];
-    const h = this.buf[0x2d];
-    const scrambled = this.readShort(0x32);
-    // eslint-disable-next-line
-    if (scrambled & 0x0004) {
-      // throw new ScrambledError;
-    }
-    json.size = { cols: w, rows: h };
-    const grid = [];
-    for (let i = 0; i < w * h; i += 1) {
-      grid.push(String.fromCodePoint(this.buf[0x34 + i]));
-    }
-    json.grid = grid;
-    this.ix = 0x33 + 2 * w * h;
-    json.title = this.readString();
-    json.author = this.readString();
-    json.copyright = this.readString();
-    const across = [];
-    const down = [];
-    const gridnums = [];
-    let label = 1;
-    for (let i = 0; i < w * h; i += 1) {
-      if (grid[i] === ".") {
-        gridnums.push(0);
-        continue;
-      }
-      let inc = 0;
-      if (i % w === 0 || grid[i - 1] === ".") {
-        across.push(`${label}. ${this.readString()}`);
-        inc = 1;
-      }
-      if (i < w || grid[i - w] === ".") {
-        down.push(`${label}. ${this.readString()}`);
-        inc = 1;
-      }
-
-      if (i % w === 0 || grid[i - 1] === ".") {
-        gridnums.push(label);
-      } else if (i < w || grid[i - w] === ".") {
-        gridnums.push(label);
-      } else {
-        gridnums.push(0);
-      }
-
-      label += inc;
-    }
-    json.clues = { across, down };
-    json.gridnums = gridnums;
-    return json;
-  }
+  json.clues = { across, down };
+  json.gridnums = gridnums;
+  return json;
 }
 
 export default async function convertPuzToJson(files) {
@@ -94,9 +93,8 @@ export default async function convertPuzToJson(files) {
         const bytes = new Uint8Array(e.target.result);
         let puz;
         if (isPuz(bytes)) {
-          puz = new PuzReader(bytes).toJson();
-        } else {
-          puz = JSON.parse(new TextDecoder().decode(bytes));
+          puz = puzToJson(bytes);
+          // puz = new PuzReader(bytes).toJson();
         }
         resolve(puz);
       };
